3001ff5c062fc6aaf7326bdbeb36cc8d25dbd6cd
BlazeKl <felipe.perezc@utem.cl>
2021-10-24 20:14:57 -0300

    proyecto en spring java maven

---
+++ climaREST/src/main/java/com0/climaREST/ClimaRestApplication.java
@@ -0,0 +1,13 @@
+package com0.climaREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimaRestApplication.class, args);
+	}
+
+}
---
+++ climaREST/src/test/java/com0/climaREST/ClimaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com0.climaREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimaRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
