086b1394622e7cfc509808e782eda2481e4b8e86
BlazeKl <felipe.perezc@utem.cl>
2021-10-29 21:13:03 -0300

    version nodejs

---
+++ climaREST/src/main/java/com0/climaREST/persistance/repository/usuariorepo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com0.climaREST.persistance.repository;
+
+import com0.climaREST.persistance.model.usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author pipe
+ */
+@Repository
+public interface usuariorepo extends JpaRepository<usuario, Long>{
+    
+}
